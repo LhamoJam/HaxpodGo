b115b609ef4262deadec2a74f4dada7bf1304907
LhamoJam <wf3597181@163.com>
2021-11-12 16:51:13 +0800

    gocv调试成功
    实例1,摄像头打开成功.

--- webCam.go
+++ webCam.go
@@ -1,64 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
-	//"github.com/gwuhaolin/livego"
-	"github.com/hybridgroup/mjpeg"
 	"gocv.io/x/gocv"
 )
 
-var (
-	deviceID int
-	err      error
-	webcam   *gocv.VideoCapture
-	stream   *mjpeg.Stream
-)
-
 func main() {
-
-
-	// parse args
-	deviceID := 0
-	host := ":8554"
-
-	// open webcam
-	webcam, err = gocv.OpenVideoCapture(deviceID)
-	if err != nil {
-		fmt.Printf("Error opening capture device: %v\n", deviceID)
-		return
-	}
-	defer webcam.Close()
-
-	// create the mjpeg stream
-	stream = mjpeg.NewStream()
-
-	// start capturing
-	go mjpegCapture()
-
-	fmt.Println("Capturing. Point your browser to " + host)
-
-	// start http server
-	http.Handle("/", stream)
-	log.Fatal(http.ListenAndServe(host, nil))
-}
-
-func mjpegCapture() {
+	webcam, _ := gocv.VideoCaptureDevice(0)
+	window := gocv.NewWindow("Hello")
 	img := gocv.NewMat()
-	defer img.Close()
 
 	for {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("Device closed: %v\n", deviceID)
-			return
-		}
-		if img.Empty() {
-			continue
-		}
-
-		buf, _ := gocv.IMEncode(".jpg", img)
-		stream.UpdateJPEG(buf)
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
 	}
 }
